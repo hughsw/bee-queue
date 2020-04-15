16c75f426ce1345fa59996fe3ffb626b5faa4fda
Hugh Secker-Walker <hsw@hodain.net>
2020-04-15 14:30:22 -0400

    feat(docker/test): Use env BEE_QUEUE_TEST_REDIS and simplify Redis usage

--- tests.sh
+++ tests.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+# Script used by the testing harness in the container run by ./docker-tests.sh
+
 set -x
 
-npx ava --no-color --serial --timeout 21000 --verbose
+npx ava --no-color --serial --timeout 30000 --verbose
 
 npm run eslint
 exit
 
-# The "official" test
+# The "official" test.  Fails when coveralls credentials aren't available
 npm test
